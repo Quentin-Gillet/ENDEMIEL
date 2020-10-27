c2f687a776fb835597bcc71726f8146aa23763ab
Mrguigui34 <guillaume.kusiak13@gmail.com>
2020-10-27 13:07:00 +0100

    add menu-feature

---
+++ public/js/js.js
@@ -0,0 +1,9 @@
+function open_submenu() {
+    document.querySelector(".submenu").style.height="100%"
+    document.querySelector(".search").style.opacity="0%"
+
+}
+function close_submenu(){
+    document.querySelector(".submenu").style.height="0%"
+    document.querySelector(".search").style.opacity="85%"
+}
--- resources/views/map/map.blade.php
+++ resources/views/map/map.blade.php
@@ -3,13 +3,14 @@
     <link href="{{ asset('css/map.css') }}" rel="stylesheet">
 @endsection
 
-<section>
-    <div class="container">
-        <div class="inner">
-            <div>
-                <h2 class="title_map">Explorer, Grimper, parmi les nombreux spots d'escalade disponibles !</h2>
+ <section>
+        <div class="container">
+            <div class="inner">
+                <div>
+                    <h2 class="title_map">Explorer, Grimper, parmi les nombreux spots d'escalde disponibles !</h2>
+                </div>
+                <div class="container_map">
+                </div>
             </div>
-            <div class="container_map"></div>
         </div>
-    </div>
-</section>
+    </section>
