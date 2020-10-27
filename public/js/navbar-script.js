f631e4461b6397304bf23a612b9486f3b8b78e1e
Mrguigui34 <guillaume.kusiak13@gmail.com>
2020-10-27 20:50:31 +0100

    fix css map-container

--- public/js/navbar-script.js
+++ public/js/navbar-script.js
@@ -1,9 +1,11 @@
 function open_submenu() {
     document.querySelector(".submenu").style.height="100%"
     document.querySelector(".search").style.opacity="0%"
+    document.querySelector(".container").style.opacity="0%"
 
 }
 function close_submenu(){
     document.querySelector(".submenu").style.height="0%"
     document.querySelector(".search").style.opacity="85%"
+    document.querySelector(".container").style.opacity="100%"
 }
