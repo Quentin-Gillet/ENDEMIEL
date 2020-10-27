e295fdda65f30b9b98abc892632b5fe816bc9537
Mrguigui34 <guillaume.kusiak13@gmail.com>
2020-10-27 19:18:36 +0100

    Add login-page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,26 @@
 {{--@extends('layouts.master')--}}
 
+@section("extra-css")
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+@show
+@endsection
 
-<form action="{{ route('login') }}" method="POST">
-    @csrf
-    <input name="email" placeholder="email">
-    <input name="password" placeholder="password">
-    <button type="submit">Submit</button>
-</form>
+<section>
+    <div class="main_container">
+        <div class="container_left">
+                <h2 class="title_login">Titre/logo</h2>
+                <form action="{{ route('login') }}" method="POST" class="form">
+                    @csrf
+                    <div class="subcontainer_login">
+                        <input type="email" class="input" placeholder="email"><br>
+                        <input type="password" class="input" placeholder="mot de passe"><br>
+                        <button class="button" type="button">Se connecter</button>
+                    </div>
+                </form>
+        </div>
+        <div class="container_right">
+             <h2 class="title_account">Tu n'as pas de compte</h2>
+             <button class="button">S'enregister</button>
+        </div>
+    </div>
+</section>
