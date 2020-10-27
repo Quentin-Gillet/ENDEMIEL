70e1391c986151451b2e61f883ed09c8794f675d
Mrguigui34 <guillaume.kusiak13@gmail.com>
2020-10-27 21:04:04 +0100

    fix scrollbar in navbar

--- public/js/navbar-script.js
+++ public/js/navbar-script.js
@@ -2,10 +2,18 @@ function open_submenu() {
     document.querySelector(".submenu").style.height="100%"
     document.querySelector(".search").style.opacity="0%"
     document.querySelector(".container").style.opacity="0%"
+    window.addEventListener('scroll',noScroll)
 
 }
 function close_submenu(){
     document.querySelector(".submenu").style.height="0%"
     document.querySelector(".search").style.opacity="85%"
     document.querySelector(".container").style.opacity="100%"
+    window.removeEventListener('scroll',noScroll)
 }
+
+function noScroll() {
+    window.scrollTo(0,0);
+}
+
+
