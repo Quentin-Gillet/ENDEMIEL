38d5e65656a25d52708f92a26d911e4f5c03734b
Mrguigui34 <guillaume.kusiak13@gmail.com>
2020-10-28 11:37:48 +0100

    fix click-button on click in compte-navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,13 +9,15 @@
                     <a><li class="lien" onclick="open_submenu()">Compte</li></a>
                         <ul class="submenu">
                             <button title="Fermer" class="button_quit" onclick="close_submenu()"><span class="icon_close"><i class="fas fa-times"></i></span></button>
-                            @guest
-                                <a href="{{ route('login') }}"><li class="lien2">Se connecter<span class="icon_login"><i class="fas fa-sign-in-alt"></i></span></li></a>
-                                <a href="{{ route('register') }}"><li class="lien2">S enregistrer<span class="icon_login"><i class="fas fa-user-plus"></i></span></li></a>
-                            @endguest
+                            <div class="block">
+                                @guest
+                                    <a href="{{ route('login') }}"><li class="lien2">Se connecter<span class="icon_login"><i class="fas fa-sign-in-alt"></i></span></li></a>
+                                    <a href="{{ route('register') }}"><li class="lien2">S' enregistrer<span class="icon_login"><i class="fas fa-user-plus"></i></span></li></a>
+                                @endguest
+                            </div>
                             @auth
                                 <a href="{{ route('user.account') }}"><li class="lien2">Mon espace<span class="icon_login"><i class="fas fa-user-circle"></i></span></li></a>
-                                <form class="form" action="{{ route('logout') }}" method="POST" id="logout_form">
+                                <form class="lien2" action="{{ route('logout') }}" method="POST" id="logout_form">
                                     @csrf
                                     <a onclick="document.getElementById('logout_form').submit();"><li class="lien2">Se déconnecter<span class="icon_login"><i class="fas fa-sign-out-alt"></i></span></li></a>
                                 </form>
