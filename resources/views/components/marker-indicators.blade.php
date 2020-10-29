0850abf5f3f1f0961c38e5077666e9ed331d4ad7
Mrguigui34 <guillaume.kusiak13@gmail.com>
2020-10-29 21:02:46 +0100

    fix bug marker indicator and

--- resources/views/components/marker-indicators.blade.php
+++ resources/views/components/marker-indicators.blade.php
@@ -18,8 +18,6 @@
                     document.querySelector(".close").style.opacity = "100%";
                     document.querySelector(".close").style.cursor = "pointer";
                 },500);
-
-
         }, 1500)
        </script>
 
@@ -29,28 +27,25 @@
     @if($errors->any())
         <div class="border_message">
             <span class="icon_error"><i class="fas fa-window-close"></i></span>
-            <div class="bar_2"></div>
             <div class="title_bar"><span class="title_2">Erreur !</span></div>
             <div class="message_success">
                 @foreach ($errors->all() as $error)
                     <li class="message_li">{{ $error }}</li>
                 @endforeach
             </div>
-            <div class="close_2">
-                <span title="Fermer" class="iconQuit_2" onclick="icon_close()"><i class="fas fa-times-circle"></i></span>
+            <div class="close">
+                <span title="Fermer" class="iconQuit" onclick="icon_close()"><i class="fas fa-times-circle"></i></span>
             </div>
         </div>
         <script>
             setTimeout(function (){
-
                 document.querySelector(".icon_error").style.opacity = "0%";
                 document.querySelector(".message_success").style.opacity = "100%";
                 setTimeout(function() {
-                    document.querySelector(".close_2").style.opacity = "100%";
-                    document.querySelector(".close_2").style.cursor = "pointer";
+                    document.querySelector(".close").style.opacity = "100%";
+                    document.querySelector(".close").style.cursor = "pointer";
                 },500);
 
             }, 1500);
-            
         </script>
     @endif
