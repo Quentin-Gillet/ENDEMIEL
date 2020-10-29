86b668c6d6c8565017166dd6aef50551286a2f84
Quentin-Gillet <quentin_gillet@orange.fr>
2020-10-29 15:33:30 +0100

    Some css fixes

--- resources/views/components/marker-indicators.blade.php
+++ resources/views/components/marker-indicators.blade.php
@@ -1,27 +1,27 @@
-{{--    Gestion des notification a success en vert    --}}
+    {{--    Gestion des notification a success en vert    --}}
     @if ($message = Session::get('success'))
-   <div class="border_message">
-        <span class="icon_success"><i class="fas fa-check-circle"></i></span>
-        <div class="title_bar"><span class="title_2">Terminé !</span></div>
-        <div class="message_success">
-            {{ $message }}
-        </div>
-        <div class="close">
-            <span title="Fermer" class="iconQuit" onclick="icon_close()"><i class="fas fa-times-circle"></i></span>
-        </div>
-   </div>
-   <script>
-        setTimeout(function (){
-            document.querySelector(".icon_success").style.opacity = "0%";
-            document.querySelector(".message_success").style.opacity = "100%";
-            setTimeout(function() {
-                document.querySelector(".close").style.opacity = "100%";
-                document.querySelector(".close").style.cursor = "pointer";
-            },500);
+       <div class="border_message">
+            <span class="icon_success"><i class="fas fa-check-circle"></i></span>
+            <div class="title_bar"><span class="title_2">Terminé !</span></div>
+            <div class="message_success">
+                {{ $message }}
+            </div>
+            <div class="close">
+                <span title="Fermer" class="iconQuit" onclick="icon_close()"><i class="fas fa-times-circle"></i></span>
+            </div>
+       </div>
+       <script>
+            setTimeout(function (){
+                document.querySelector(".icon_success").style.opacity = "0%";
+                document.querySelector(".message_success").style.opacity = "100%";
+                setTimeout(function() {
+                    document.querySelector(".close").style.opacity = "100%";
+                    document.querySelector(".close").style.cursor = "pointer";
+                },500);
 
-        }, 1500);
+            }, 1500);
 
-   </script>
+       </script>
    @endif
 
     {{--    Gestion des notifications avec erreurs en rouge    --}}
@@ -40,11 +40,9 @@
             </div>
         </div>
         <script>
-                setTimeout(function (){
-                        document.querySelector(".close_2").style.opacity = "100%";
-                        document.querySelector(".close_2").style.cursor = "pointer";
-                    },500);
-
-
+            setTimeout(function (){
+                    document.querySelector(".close_2").style.opacity = "100%";
+                    document.querySelector(".close_2").style.cursor = "pointer";
+                },500);
         </script>
     @endif
